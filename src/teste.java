4f2875f1f6d343ca83c8f570519bf6e28ac699e0
LucasSCarmo0305 <lucas0305carmo2007@gmail.com>
2026-01-14 14:57:01 -0300

    Modelagem UML e implementação do iPhone

---
+++ src/teste.java
@@ -0,0 +1,9 @@
+public class teste {
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+
+        iphone.tocar();
+        iphone.ligar("99999-9999");
+        iphone.exibirPagina("www.apple.com");
+    }
+}
